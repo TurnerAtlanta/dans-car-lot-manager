656763cd7fe4d474796b2394ff7ecb90449d5025
James Michael Hunter Turner <michael@turneratlanta.com>
2025-10-22 18:43:02 -0400

    Create TimeLog.jsx

---
+++ src/components/TimeLog.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function TimeLog() {
+  return (
+    <section>
+      <h2>Time Tracking View</h2>
+      {/* Add your time tracking UI here */}
+    </section>
+  )
+}
