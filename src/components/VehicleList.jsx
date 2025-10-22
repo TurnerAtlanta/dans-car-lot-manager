3f1b1e9157cfcb13787e2c0a114c20237cd73ee3
James Michael Hunter Turner <michael@turneratlanta.com>
2025-10-22 18:44:11 -0400

    Create VehicleList.jsx

---
+++ src/components/VehicleList.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function VehicleList() {
+  return (
+    <section>
+      <h2>Vehicle Inventory</h2>
+      {/* Add vehicle list UI and photos */}
+    </section>
+  )
+}
