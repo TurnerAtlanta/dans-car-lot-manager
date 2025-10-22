0f480464e88319bd77da7f841dc71b5dffcd5e45
James Michael Hunter Turner <michael@turneratlanta.com>
2025-10-22 00:57:28 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+function CarLotManager() {
+  return (
+    <div>
+      <h1>Car Lot Manager</h1>
+      <p>Hello from React!</p>
+    </div>
+  );
+}
+ReactDOM.createRoot(document.getElementById('root')).render(<CarLotManager />);
