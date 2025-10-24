a9f606eac3e8c1f6c31c0453cd2747b7b9247311
James Michael Hunter Turner <michael@turneratlanta.com>
2025-10-24 19:54:00 -0400

    Update calculations.js

--- src/utils/calculations.js
+++ src/utils/calculations.js
@@ -1,1 +1,5 @@
+import { Sale, Maintenance } from '../types';
 
+export const calculateTotalSales = (sales: Sale[]): number => sales.reduce((sum, s) => sum + Number(s.salePrice), 0);
+
+export const calculateTotalCosts = (maintenances: Maintenance[]): number => maintenances.reduce((sum, m) => sum + Number(m.cost), 0);
